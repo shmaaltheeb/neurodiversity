884883d718a150d244786f560f8dda8747415aba
meowkio <111453469+meowkio@users.noreply.github.com>
2022-08-18 19:41:05 +0100

    pop up

--- neurodiversity/ViewController.swift
+++ neurodiversity/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meetDevButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.meetDevButton.layer.cornerRadius = 25
     }
 
 
---
+++ neurodiversity/meetDevPopup.swift
@@ -0,0 +1,13 @@
+//
+//  meetDevPopup.swift
+//  neurodiversity
+//
+//  Created by Chloe Loca on 18/08/2022.
+//
+
+import UIKit
+
+class meetDevPopup: UIView {
+
+
+}
