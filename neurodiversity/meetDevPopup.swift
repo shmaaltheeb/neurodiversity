ad2c6c916338b529e5d1d0cf09e41ef46c92e545
meowkio <111453469+meowkio@users.noreply.github.com>
2022-08-18 20:22:33 +0100

    pop up - incomplete

--- neurodiversity/meetDevPopup.swift
+++ neurodiversity/meetDevPopup.swift
@@ -9,5 +9,28 @@ import UIKit
 
 class meetDevPopup: UIView {
 
+    @IBOutlet weak var closePopup: UIButton!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override.init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        
+    }
+    
+    func xibSetup(frame: CGRect){
+        let view = loadXib()
+        view.frame = frame
+        addSubview(view)
+    }
 
+    func loadXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "PopUp", bundle)
+        
+    }
+    
 }
